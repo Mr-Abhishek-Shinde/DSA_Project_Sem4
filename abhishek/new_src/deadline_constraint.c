f577c5b4bb9f15a78c81ac5b404af09729d94194
abhishek <shindeaa21.comp@coep.ac.in>
2023-05-28 01:16:44 +0530

    created function for insertion operator and modified file structure

---
+++ abhishek/new_src/deadline_constraint.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <float.h>
+#include "constraints.h"
+#include "time_functions.h"
+#include "mathematics.h"
+
+
+int dis_from_origin[NNODE];
+
+// distance between given index
+double dis_index(location_node *a, location_node *b){
+		return (dis_from_origin[b->index] - dis_from_origin[a->index]);
+}
+
+// ddl(k) function - latest time to arrive at lk without violating the deadline constraints
+double ddl(location_node *k){
+		double total_time;
+		Request *k_request = k->corresponding_request;
+
+		//if((k_request->origin.x == k->sequenced_location.x) && (k_request->origin.y == k->sequenced_location.y)){
+		if(k_request->origin == k){
+			//	total_time = k_request->deadline_time - dis(k_request->origin, k_request->destination);
+				total_time = k_request->deadline_time - dis_index(k, k_request->destination);
+		}
+		else if(k_request->destination == k){
+				total_time = k_request->deadline_time;
+		}
+		return total_time;
+}
+				
+// det(k, p) - the detour time of inserting location p after lk
+double det(location_node *k, location_node *p){
+		double detour;
+        if(!k->next_location_node){
+            return distance_node(k->sequenced_location, p->sequenced_location);
+        }
+		detour = distance_node(k->sequenced_location, p->sequenced_location) + distance_node(p->sequenced_location, k->next_location_node->sequenced_location);// - distance_node(k->sequenced_location, k->next_location_node->sequenced_location);
+		return detour;
+}
+
+// arr(k) (arrival time) - it will be time from origin to current node
+double arr(location_node *k, Request newRequest){
+		location_node *p = ridesharing.route.path;
+		int arr_time;
+		arr_time = dis_index(p, k);   // Route means from Worker to k
+		arr_time += newRequest.release_time;
+		return arr_time;
+}
+
+// insertion of a node after b 
+void insert(location_node *a, location_node *b){
+		location_node *temp = b->next_location_node;
+		b->next_location_node = a;
+		a->next_location_node = temp;
+		return;
+}
+ 
