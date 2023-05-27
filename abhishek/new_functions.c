552b99bdfb0ea1692bc4389684db980628bcc349
abhishek <shindeaa21.comp@coep.ac.in>
2023-05-27 22:45:14 +0530

    updated mobj function

--- abhishek/new_functions.c
+++ abhishek/new_functions.c
@@ -1,22 +1,39 @@
-// precalculating mobj:
-void mobj(Route r, double **mobj_values, Request newRequest){
-        location_node *p = r.path;
-        while(p->next_location_node){
-                p = p->next_location_node;
-        }
+void mobj(Route r, double *mobj_values, Request newRequest){
+	location_node *p = r.path;
+	int n = r.path.no_of_nodes;
 
-        double ft_curr = flow_time(r, *(p->corresponding_request), newRequest);
-        int n = r.path.no_of_nodes;
+	mobj_helper(r, p, mobj_values, newRequest, n, 0);
 
-        for(int i = n; i > 0; i--){
-                if(i = n){
-                        mobj[i][n] = 0;
+	return;
+}
+
+void mobj_helper(Route r, location_node *li, double *mobj_values, Request newRequest, int n, int i){
+	if(i == n - 1){
+		mobj_values[i] = flow_time(r, *(p->corresponding_request), newRequest);
+		return;
+	}
+	else{
+		double ft_curr;
+
+		if(li == li->corresponding_request->destination){
+			ft_curr = flow_time(r, *(q->corresponding_request), newRequest);
                 }
                 else{
-                        mobj[i][n] = max(ft_curr ,mobj[i + 1][n]);
-                }
-        }
+                	ft_curr = 0;
+		}
 
-        return;
+		mobj_helper(r, li->next_location_node, mobj_values, newRequest, n, i + 1);
+		mobj_values[i] = max(ft_curr, mobj_values[i + 1]);
+	}
+
+	return;
 }
+
+
+
+
+
+
+
+
 
