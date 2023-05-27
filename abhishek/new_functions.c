6b83b5de210694257819807c859fd34f630d75dd
abhishek <shindeaa21.comp@coep.ac.in>
2023-05-27 23:55:21 +0530

    created a function to do modified binary seasearch

--- abhishek/new_functions.c
+++ abhishek/new_functions.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 void mobj(Route r, double *mobj_values, Request newRequest){
 	location_node *p = r.path;
 	int n = r.path.no_of_nodes;
@@ -30,7 +32,46 @@ void mobj_helper(Route r, location_node *li, double *mobj_values, Request newReq
 }
 
 
+// binary search in sorted array:
+int binary_search_thr(double *thr_arr, int key, int i, int n){
+	int l = i;
+	int h = n;
+	int m;
 
+	while(l <= h){
+		m = (l + h) / 2;
+
+		if(thr_arr[m] == key){
+			return m;
+		}
+		else if(thr_arr[m] > key && (m != 0 && thr_arr[m - 1] < key)){
+			return m;
+		}
+		else{
+			if(thr_arr[m] < key){
+				l = m + 1;
+			}
+			else{
+				h = m - 1;
+			}
+		}
+	}
+
+	return 0;
+}	
+
+int main(){
+	double arr[] = {2.3, 4.3, 6.2, 9.7, 9.9, 12.5, 31.3, 21.5};
+
+	printf("%d\n", binary_search_thr(arr, 6.2, 0, 7));
+	printf("%d\n", binary_search_thr(arr, 6.2, 3, 7));
+	printf("%d\n", binary_search_thr(arr, 5.1, 0, 7));
+	printf("%d\n", binary_search_thr(arr, 31.3, 0, 7));
+	printf("%d\n", binary_search_thr(arr, 1.2, 0, 7));
+	printf("%d\n", binary_search_thr(arr, 2.4, 6, 7));
+
+	return 0;
+}
 
 
 
