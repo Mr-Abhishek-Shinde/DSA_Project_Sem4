d99e7cf7ca2458eb8a9a4dd7b7c03dc2f4b1548a
amanasayyad2003 <amanaslamsayyad2003@gmail.com>
2023-05-28 12:40:50 +0530

    Make file, tools.h, segment tree file added in Algorithm 4 directory

---
+++ Algorithm4/include/tools.h
@@ -0,0 +1,28 @@
+#ifndef TOOLS
+
+#define TOOLS
+
+#include "structures.h"
+
+/* a raise to b - a^b */
+double a_raise_b(double a, double b);
+
+ /* find distance between a and b coordinate */
+double distance_node(coordinate a, coordinate b);
+
+ /* find min value between num1 and num2 */
+double min(double num1, double num2);
+
+/* returns max of a and b */
+int max(int a, int b);
+
+/* scale the unit_vector according to scale input */
+void scale_vector(coordinate *result, coordinate unit_vector, double scale);
+
+/* addition of two vector */
+void add_vector(coordinate *result, coordinate a, coordinate b);
+
+/* will find unit vector between a and b vector */
+void find_unit_vector(coordinate *result,coordinate a, coordinate b);
+
+#endif
---
+++ Algorithm4/src/tools.c
@@ -0,0 +1,53 @@
+#include "tools.h"
+#include "math.h"
+
+/* a raise to b - a^b */
+double a_raise_b(double a, double b){
+		double result = a;
+		for(int i = 0; i < (b - 1); i++){
+				result = result * a;
+		}
+		return result;
+}
+
+ /* find distance between a and b coordinate */
+double distance_node(coordinate a, coordinate b){
+		double distance, delta_x, delta_y;
+		delta_x = a.x - b.x;
+		delta_y = a.y - b.y;
+		distance = sqrt(a_raise_b(delta_x, 2) + a_raise_b(delta_y, 2));
+		return distance;
+}
+
+ /* find min value between num1 and num2 */
+double min(double num1, double num2){
+		return (num1 > num2 ? num2 : num1);
+}
+
+/* returns max of a and b */
+int max(int a, int b){
+	return (a > b ? a : b);
+}
+
+/* scale the unit_vector according to scale input */
+void scale_vector(coordinate *result, coordinate unit_vector, double scale){
+		(*result).x = unit_vector.x * scale;
+		(*result).y = unit_vector.y * scale;
+		return;
+}
+
+/* addition of two vector */
+void add_vector(coordinate *result, coordinate a, coordinate b){
+		(*result).x = a.x + b.x;
+		(*result).y = a.y + b.y;
+		return;
+}
+
+/* will find unit vector between a and b vector */
+void find_unit_vector(coordinate *result, coordinate a, coordinate b){
+		double magnitude = distance_node(a, b); 
+		result->x = (b.x - a.x) / magnitude;
+		result->y = (b.y - a.y) / magnitude;
+		return;
+}
+
