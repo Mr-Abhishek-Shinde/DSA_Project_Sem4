e4992a4cf4841ef66d6e4cf035bc5f4af9d9e251
bhushan.shah05 <bhushan.shah05@gmail.com>
2023-05-28 13:42:14 +0530

    minor change in constraints file

--- Algorithm4/src/constraints.c
+++ Algorithm4/src/constraints.c
@@ -2,7 +2,7 @@
 #include "structures.h"
 //New function for capacity for handling i=j cases
 int check_capacity_constraint_iEqualj(Request new_request, double *pck_values, int i){
-	return pck_values[i] <= (ridesharing_state.w.capacity - new_request.capacity);
+	return pck_values[i] <= (ridesharing_state.worker.capacity - new_request.capacity);
 }
 
 //New function for deadline for handling i=j cases
